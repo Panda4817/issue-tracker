a7fdce56085d2e3a0774ee13073f11f8da0dd937
K Munton <kanta.munton@gmail.com>
2020-11-06 10:30:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+**FreeCodeCamp**- Quality Assurance project 2
+------
+[![Run on Repl.it](https://repl.it/badge/github/freeCodeCamp/boilerplate-project-issuetracker)](https://repl.it/github/freeCodeCamp/boilerplate-project-issuetracker)
+## Issue Tracker Project
+
+- All the logic is in `routes/api.js`.
+- I will create the functional tests in `tests/2_functional-tests.js`.
+
+## Usage
+
+- SET NODE_ENV to `test` without quotes and set DB to your mongo connection string in .env file
+- Run `npm start`
+
+
---
+++ routes/api.js
@@ -0,0 +1,49 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+var expect = require('chai').expect;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+const CONNECTION_STRING = process.env.DB;
+const client = new MongoClient(CONNECTION_STRING, {useUnifiedTopology: true, useNewUrlParser: true});
+client.connect(err => {
+    var db = client.db('test')
+    db.collection('issue').find().toArray(function (err, result) {
+        if (err) throw err
+        console.log(result);
+    })
+})
+
+module.exports = function (app) {
+
+  app.route('/api/issues/:project')
+  
+    .get(function (req, res){
+      var project = req.params.project;
+      
+    })
+    
+    .post(function (req, res){
+      var project = req.params.project;
+      
+    })
+    
+    .put(function (req, res){
+      var project = req.params.project;
+      
+    })
+    
+    .delete(function (req, res){
+      var project = req.params.project;
+      
+    });
+    
+};
